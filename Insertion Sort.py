9b4c463a5615a5995aca006e83c8b2abdd52fca0
Rohan <rohanpatil9529@gmail.com>
2021-07-27 23:53:04 +0530

    Bubble Sort

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -7,7 +7,7 @@ def insertionSort(arr):
       while j >=0 and key < arr[j] :
          arr[j+1] = arr[j]
          j -= 1          
-       arr[j+1] = key
+      arr[j+1] = key
 # main
 arr = ['t','u','t','o','r','i','a','l']
 insertionSort(arr)
